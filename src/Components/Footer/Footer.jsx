720375a800f19095b3a10f9709fc114434540bd3
giangnht19 <s223197048@deakin.edu.au>
2024-08-14 22:50:44 +1000

    Fixed Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -17,6 +17,7 @@ const Footer = () => {
         <li>Products</li>
         <li>Officer</li>
         <li>Services</li>
+        <li>Programs</li>
         <li>About</li>
         <li>Contact</li>
         <li>Careers</li>
