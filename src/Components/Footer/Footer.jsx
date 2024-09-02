09b79dd8c1b4daa8e7a6495abc1fed4ea8854cd5
Huu Truong Giang Nguyen <s223197048@deakin.edu.au>
2024-09-02 14:57:09 +1000

    Update Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -16,7 +16,6 @@ const Footer = () => {
         <li>Company</li>
         <li>Products</li>
         <li>Officer</li>
-        <li>Services</li>
         <li>About</li>
         <li>Contact</li>
         <li>Careers</li>
